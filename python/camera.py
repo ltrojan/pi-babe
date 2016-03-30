a2883bf7e29e0e1a300b3635219b56733c1598af
lorenzo <ltrojan@mintel.com>
2016-03-30 21:54:08 +0100

    Store one pi-cam image (with cv2)

--- python/camera.py
+++ python/camera.py
@@ -1,9 +1,11 @@
 import StringIO
 import picamera
+import cv2
 
 
 if __name__ == "__main__":
     cam = picamera.PiCamera()
     buf = StringIO.StringIO()
     cam.capture(buf, 'png')
+    cv2.imwrite('test.png', buf)
     cam.close()
